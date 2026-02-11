8be43979e95742c87eaab65afc261a96efab82f3
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-02-11 12:31:13 -0800

    playwright, vitest, RTL installed in app

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function Welcome() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Welcome to my React App!</h1>
+      <p>This is the welcome page.</p>
+      <button onClick={() => navigate("/resume")}>Go to Resume</button>
+      <button onClick={() => navigate("/explorer")}>Go to Explorer</button>
     </div>
   );
 }
