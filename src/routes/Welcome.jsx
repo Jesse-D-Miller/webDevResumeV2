2a7a654705704249d94908396c4ff26a853c49bb
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-02-11 15:53:34 -0800

    welcome page css file created

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,14 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import "./Welcome.css";
 
 function Welcome() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Welcome to my React App!</h1>
-      <p>This is the welcome page.</p>
-      <button onClick={() => navigate("/resume")}>Go to Resume</button>
-      <button onClick={() => navigate("/explorer")}>Go to Explorer</button>
+    <div className="welcome">
+      <h1 className="welcome__title">Welcome to my React App!</h1>
+      <p className="welcome__text">This is the welcome page.</p>
+      <div className="welcome__actions">
+        <button
+          className="welcome__button"
+          onClick={() => navigate("/resume")}
+        >
+          Go to Resume
+        </button>
+        <button
+          className="welcome__button"
+          onClick={() => navigate("/explorer")}
+        >
+          Go to Explorer
+        </button>
+      </div>
     </div>
   );
 }
