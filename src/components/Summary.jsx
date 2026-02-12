ef2a43bc331bf056f7eb1a4634e5a9fe36b0e04c
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-02-12 11:28:12 -0800

    character summary page and css

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,11 @@
+import "./Summary.css";
+import data from "../data/resume.json";
+
 function Summary() {
   return (
     <div className="summary">
-      <h1 className="summary-title">Summary Page</h1>
+      <h1 className="summary-title">Character Summary</h1>
+      <p className="summary-text">{data.summary}</p>
     </div>
   );
 }
