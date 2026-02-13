fe4d1fd6f425bcb2a4f32cdd4114ac0821a2d1b5
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-02-13 10:27:30 -0800

    gear data and jsx file edited

--- src/components/Gear.jsx
+++ src/components/Gear.jsx
@@ -1,64 +1,17 @@
 import "./Gear.css";
+import gear from "../data/gear.json";
 
 function Gear() {
   return (
     <div className="gear">
       <ul className="gear-list">
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Helmet</span>
-          <span className="gear-level">lvl 1</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Armor</span>
-          <span className="gear-level">lvl 5</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Weapon</span>
-          <span className="gear-level">lvl 10</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Shield</span>
-          <span className="gear-level">lvl 15</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Boots</span>
-          <span className="gear-level">lvl 20</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Gloves</span>
-          <span className="gear-level">lvl 25</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Accessory</span>
-          <span className="gear-level">lvl 30</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Ring</span>
-          <span className="gear-level">lvl 35</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Amulet</span>
-          <span className="gear-level">lvl 40</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Belt</span>
-          <span className="gear-level">lvl 45</span>
-        </li>
-        <li className="gear-item">
-          <span className="gear-img">IMG</span>
-          <span className="gear-name">Cloak</span>
-          <span className="gear-level">lvl 50</span>
-        </li>
+        {gear.items.map((item) => (
+          <li key={item.id} className="gear-item">
+            <span className="gear-img">{item.img}</span>
+            <span className="gear-name">{item.name}</span>
+            <span className="gear-level">lvl {item.level}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
